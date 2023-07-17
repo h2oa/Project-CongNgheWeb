0bcfa34b4977801ba6dbf5da0604ac15fcbfae11
lengochoahust <114990730+lengochoahust@users.noreply.github.com>
2023-07-17 21:01:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,24 +4,11 @@ session_start();
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit();
+} else {
+    header("Location: dashboard.php");
+    exit();
 }
 
 // Content của index.php
 // Sửa thêm
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Home</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-<body>
-    <div class="container">
-        <h1>Welcome to the Home Page</h1>
-        <p>Hello, <?php echo $_SESSION['username']; ?>!</p>
-        <a href="logout.php">Logout</a>
-    </div>
-</body>
-</html>
